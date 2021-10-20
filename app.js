982d7c161f6909f4c51663a311b57175f24cc4c2
Ben Graves <bengraves2000@gmail.com>
2021-10-20 17:55:18 +0100

    Add a front-end

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 const tuneBot = require('./tuneBot');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 
 var bot = new tuneBot();
 
-(async () => {
-    await bot.init('wyre');
-    await bot.run();
-})();
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.post('/play', (req, res) => {
+    const roomName = req.body.roomName;
+    (async () => {
+        await bot.init(roomName);
+        await bot.run();
+    })();
+    res.render('playing');
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
